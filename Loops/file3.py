a455733619938ed7099d5ce15f1fa6c6aa362f85
samuelhenga <divstack03@gmail.com>
2024-03-25 20:11:46 -0500

    update loop folder

---
+++ Loops/file3.py
@@ -0,0 +1,25 @@
+# nested loop
+
+# adj = ["red", "big","green"]
+# fruits = ["apple", "pineapple","cherry"]
+
+# for desc in adj:
+#     for frt in fruits:
+#      #   print(desc, frt)
+
+# # # Break the loop
+# for x in range(6):
+#     if x == 3: break
+#     # print(x)
+# else:
+#     # print("Code finished")
+        
+cars = ["Volvo", "Maseratti", "Toyota", "BMW"]
+for car in cars:
+    if car == "Maseratti":
+        continue
+    print(car)
+
+
+
+
